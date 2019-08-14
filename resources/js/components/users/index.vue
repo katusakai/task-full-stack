8183b68c6729871bfe6785134022a91d1340fa4a
TadasJanca <tadasjanca@gmail.com>
2019-08-14 15:33:55 +0300

    created vue component to see all users

---
+++ resources/js/components/users/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{users}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                users: {}
+            }
+        },
+
+        mounted() {
+            this.loadUsers();
+        },
+
+        methods: {
+            loadUsers() {
+                axios.get('/users')
+                    .then(response => {
+                        this.users = response.data
+                    })
+            }
+        }
+    }
+</script>
