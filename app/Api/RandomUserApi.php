bb6ec8a06990a78f3c2356136b6350c80c1dabc6
TadasJanca <tadasjanca@gmail.com>
2019-08-14 14:48:30 +0300

    added users API and seeder

---
+++ app/Api/RandomUserApi.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\API;
+
+
+class RandomUserApi
+{
+    protected $name;
+
+    protected $email;
+
+    protected $avatar;
+
+    public function __construct()
+    {
+        $json = $this->json_decode();
+
+        $this->name = $json->name->first . '' . $json->name->last;
+        $this->avatar = $json->picture->large;
+        $this->email = $json->email;
+    }
+    protected function json_decode()
+    {
+        $json = file_get_contents('https://randomuser.me/api/');
+        return json_decode($json)->results[0];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+}
